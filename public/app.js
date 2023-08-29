9a031aa4ba7bba0dc462a4ea23da7c0b8baf26b0
Rinat273 <bairam0487@gmail.com>
2023-08-30 01:19:25 +0400

    Add add_to_cart function stub

--- public/app.js
+++ public/app.js
@@ -1,12 +1,17 @@
 function something()
 {
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-	x = x * 1 + 1;
+	x = x * 1 + 1; // x = x + 1
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
 	alert(x);
 
 	// переменная х сохраняется при переходе между окнами
+}
+
+function add_to_cart()
+{
+	alert('Hello from function!');
 }
